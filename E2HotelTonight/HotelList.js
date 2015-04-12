2be8421292577c83e69eadad6c7f7cafd761ad8b
Shaohua <shaohua@users.noreply.github.com>
2015-04-12 14:32:48 -0700

    Add renderRow

--- E2HotelTonight/HotelList.js
+++ E2HotelTonight/HotelList.js
@@ -5,15 +5,30 @@ var {
   Text,
   View,
   Image,
+  ListView,
   ScrollView,
   StyleSheet,
   TouchableHighlight,
 } = React;
 
 var HotelList = React.createClass({
+  getInitialState: function() {
+    var ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    return {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  },
+
+
   render: function() {
     return (
       <View style={styles.container}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData}</Text>}
+        />
       </View>
     );
   }
