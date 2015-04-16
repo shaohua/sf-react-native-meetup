32546df73d22c435c44236a06b069d0949434efe
Shaohua Zhou <shaohua@uber.com>
2015-04-15 23:44:42 -0700

    Add fetch call to get data from wikipedia

--- E3WeChat/ContactDetail.js
+++ E3WeChat/ContactDetail.js
@@ -12,16 +12,34 @@ var {
   TouchableHighlight,
 } = React;
 
+var fetch = require('fetch');
+
 var ContactDetail = React.createClass({
   getInitialState: function() {
-    return {
-    };
+    return {};
+  },
+
+  componentDidMount: function() {
+    var url = 'https://en.wikipedia.org/w/api.php?format=json&action=query'
+      + '&prop=extracts&exintro=&explaintext=&titles='
+      + encodeURIComponent(this.props.rowData);
+
+    fetch(url)
+      .then((response) => {
+        this.setState({
+          response: response
+        });
+      })
+      .catch((error) => {
+        console.error('error');
+      });
   },
 
   render: function() {
     return (
       <View style={styles.container}>
         <Text>{this.props.rowData}</Text>
+        <Text>{this.state.response}</Text>
       </View>
     );
   }
