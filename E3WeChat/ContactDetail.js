c6fbb10b1dc9f5ff16a2deed76bb3bf875cdd850
Shaohua Zhou <shaohua@uber.com>
2015-04-15 22:42:45 -0700

    Style ContactList and add dummy ContactDetail

---
+++ E3WeChat/ContactDetail.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+} = React;
+
+var ContactDetail = React.createClass({
+  getInitialState: function() {
+    return {
+    };
+  },
+
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text>test</Text>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    marginTop: 64,
+  },
+});
+
+module.exports = ContactDetail;
--- E3WeChat/ContactTabMain.js
+++ E3WeChat/ContactTabMain.js
@@ -10,23 +10,59 @@ var {
   Text,
   View,
   NavigatorIOS,
+  ListView,
+  TouchableHighlight,
 } = React;
+
+var ContactDetail = require('./ContactDetail');
 
 var ContactTabMain = React.createClass({
   getInitialState: function() {
+    var ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
     return {
+      dataSource: ds.cloneWithRows([
+        'Larry Page', 'Steve Jobs', 'Sergey Brin',
+        'Evan Williams', 'Jack Dorsey', 'Jerry Yang',
+        'Paul Allen', 'Reid Hoffman', 'Jack Ma', 'Elon Musk',
+        'Sean Parker', 'Mark Zuckerberg', 'Larry Ellison', 'Lei Jun',
+        'Robin Li', 'Ma Huateng']),
     };
+  },
+
+  _pressRow: function() {
+    // AlertIOS.alert('console.log?', 'NO!');
+    this.props.navigator.push({
+      title: 'Contact Detail',
+      component: ContactDetail,
+      passProps: {}
+    });
+  },
+
+  _renderRow: function(rowData) {
+    return (
+      <View style={styles.rowContainer}>
+        <TouchableHighlight
+          onPress={() => this._pressRow()}
+          underlayColor='deepskyblue'
+          style={styles.rowButton}>
+          <Text>{rowData}</Text>
+        </TouchableHighlight>
+        <View style={styles.rowAction}>
+          <Text>Call</Text>
+        </View>
+      </View>
+    )
   },
 
   render: function() {
     return (
       <View style={styles.container}>
-        <View style={styles.contactList}>
-          <Text>Adam Smith</Text>
-        </View>
-        <View style={styles.contactList}>
-          <Text>Larry Page</Text>
-        </View>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderRow}
+        />
       </View>
     );
   }
@@ -34,13 +70,25 @@ var ContactTabMain = React.createClass({
 
 var styles = StyleSheet.create({
   container: {
-    paddingTop: 70,
     flex: 1,
-  },
-  contactList: {
     backgroundColor: 'bisque',
+  },
+  rowContainer: {
+    flexDirection: 'row',
     height: 50,
+  },
+  rowButton: {
+    flex: 3,
+    alignItems: 'center',
     justifyContent: 'center',
+  },
+  rowAction: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 5,
   }
 });
 
