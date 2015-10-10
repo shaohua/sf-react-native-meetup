009cdab9a09686e6274d55d6cc690f2ad2dee83e
Shaohua <shaohua@users.noreply.github.com>
2015-10-09 23:12:53 -0700

    Add icons

--- E4WeChatAndroid/android/app/src/main/java/com/e4wechatandroid/MainActivity.java
+++ E4WeChatAndroid/android/app/src/main/java/com/e4wechatandroid/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
                 .setBundleAssetName("index.android.bundle")
                 .setJSMainModuleName("index.android")
                 .addPackage(new MainReactPackage())
+                .addPackage(new ReactNativeIcons())
                 .setUseDeveloperSupport(BuildConfig.DEBUG)
                 .setInitialLifecycleState(LifecycleState.RESUMED)
                 .build();
